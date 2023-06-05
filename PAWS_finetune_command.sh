b3cf25568b8a3d749cfeea7f881dda30a94c8595
ARBEZ Loic <loic.arbez@inria.fr>
2023-06-05 10:42:56 +0200

    Useless minor changes

--- PAWS_finetune_command.sh
+++ PAWS_finetune_command.sh
@@ -4,4 +4,4 @@ source gpu_setVisibleDevices.sh
 GPUID=0
 cd /home/larbez/Documents/suncet/
 
-python main.py --sel fine_tune --fname configs/paws/ClusteVec_finetune_imgnt.yaml #configs/paws/ClusteVec_finetune.yaml #
+python main.py --sel fine_tune --fname configs/paws/ClusteVec_finetune.yaml #configs/paws/ClusteVec_finetune.yaml #
